c7a186d4c4505dc554b4bd3b0bf8a26132e23032
shijie <shijie@shijie.(none)>
2019-04-21 12:05:26 +0800

    tab align

--- link_list/link_list.c
+++ link_list/link_list.c
@@ -5,17 +5,17 @@ link_list_t* reverse_list(link_list_t* p_head)
     link_list_t *p = NULL, *q = NULL, *p_next = NULL;
     if(NULL == p_head)
     {
-	return p_head;
+    	return p_head;
     }
 
     p = p_head;
     p_next = p->p_next;
     while(p_next)
     {
-	q = p_next->p_next;
-	p_next->p_next = p;
-	p = p_next;
-	p_next = q;
+		q = p_next->p_next;
+		p_next->p_next = p;
+		p = p_next;
+		p_next = q;
     }
     p_head->p_next = NULL;
 
@@ -26,13 +26,13 @@ void print_list(link_list_t* p_head)
 {
     if(NULL == p_head)
     {
-	printf("empty list");
+    	printf("empty list");
     }
 
     while(p_head)
     {
-	printf("%d\t", p_head->data);
-	p_head = p_head->p_next;
+		printf("%d\t", p_head->data);
+		p_head = p_head->p_next;
     }
 
     printf("\n");
