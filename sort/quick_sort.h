5c73bcdd99bac1f53b87b611eeebda9e4946300d
shi.jie <bravetroop@163.com>
2019-12-19 09:39:20 +0800

    Update quick_sort.h

--- sort/quick_sort.h
+++ sort/quick_sort.h
@@ -1,12 +1,12 @@
 #ifndef _QUICK_SORT_H_
 #define _QUICK_SORT_H_
 
-void print_arr(int *parr, int len);
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+void print_arr(int *parr, int len);
 
 void quick_sort(int *parr, int len);
 
